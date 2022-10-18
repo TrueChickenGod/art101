4de7181de44744c91eccd27bd493b779b327b90c
TrueChickenGod <dattaramola@gmail.com>
2022-10-17 20:54:38 -0700

    added and changed according to assignment, fingers crossed it worked but probs not

---
+++ lab5/js/lab.js
@@ -0,0 +1,30 @@
+/**
+  * Author (s): Ramola Datta <rsdatta@ucssc.edu>
+  *           : Kiyo ____
+  * Created: 10.17.2022
+  *
+  *
+  **/
+
+//Define Variables
+  make = "Chrysler";
+
+  model = "Pacifica";
+
+  color = "grey";
+
+  year = "2006";
+
+//calculate
+  age = 2022 - year;
+
+//output
+  document.writeln("Make: " + make + "<br>");
+
+  document.writeln("Model: " + model + "<br>");
+
+  document.writeln("Color: " + color + "<br>");
+
+  document.writeln("Year: " + year + "<br>");
+
+  document.writeln("Age: " + age + " years<br>");
