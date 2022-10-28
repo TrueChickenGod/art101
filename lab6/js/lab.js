9d32181c9c6803ac1de41e1502b43935f90bac91
TrueChickenGod <dattaramola@gmail.com>
2022-10-27 18:17:38 -0700

    added screenshots and text as per instruction of file on Canvas

--- lab6/js/lab.js
+++ lab6/js/lab.js
@@ -1,6 +1,24 @@
 /**
- * Author: Chicken God
- * Created: 10.27.2022
- *
- *
+ * Author: Ramola Datta <rsdatta@ucsc.edu>
+ * Created: Oct 20, 2022
+ * Arrays and Objects
  **/
+
+
+//define variables
+
+ myTransport = ["Niisan Leaf", "Bus", "Bike", "Walking"];
+
+ myMainRide = {
+   make:"Niisan",
+   model:"Leaf",
+   color:"white",
+   year:"2019"
+ };
+
+ //output
+
+ document.writeln("How I get around:", myTransport);
+
+ document.writeln("My Main Ride: <pre>",
+    JSON.stringify(myMainRide, null, '\t'), "</pre>");
