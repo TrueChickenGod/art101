85e5c7020f6d5f47c78f20bce1ecbc6bd620d685
TrueChickenGod <dattaramola@gmail.com>
2022-10-28 22:07:36 -0700

    changed and images added as well as text and an attempt at learning which didn't go over too well honestly

--- lab7/js/lab7.js
+++ lab7/js/lab7.js
@@ -5,4 +5,9 @@
  *
  **/
 
- 
+//creating a function, new name, not sure what it's going to do so i will name it attemptOne
+
+function attemptOne () {
+  var userName = window.prompt("Yo! You need some help with your name? I gotcha! What is it?");
+
+}
