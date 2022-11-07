e25d112bf874d89b955f17a1aeedb061ada36869
TrueChickenGod <dattaramola@gmail.com>
2022-11-07 14:21:02 -0800

    added shtuffs

---
+++ lab9/js/lab.js
@@ -0,0 +1,8 @@
+/*
+ * Author(s): Kiyo Leong <maaleong@ucsc.edu>
+              Ramola Datta <rsdatta@ucsc.edu>
+ * Created: Nov. 7, 2022
+ * Lab 9: JavaScript for the Web
+ */
+
+var outputEl = getElementById('output')
