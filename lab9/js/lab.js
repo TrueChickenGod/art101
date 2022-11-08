315ddfa1258e4d1b3882bd528aea4f4b16cb46b4
TrueChickenGod <dattaramola@gmail.com>
2022-11-07 19:27:22 -0800

    wrote the code n stuff

--- lab9/js/lab.js
+++ lab9/js/lab.js
@@ -5,4 +5,16 @@
  * Lab 9: JavaScript for the Web
  */
 
-var outputEl = getElementById('output')
+var outputEl = getElementById('output');
+
+var new1El = document.createElement("p");
+    new1El.innerHTML = "Random";
+
+var new2El = document.createElement("p");
+    new2El.innerHTML = "Again";
+
+//adding new DOM elements?
+
+outputEl.appendChild(new1El);
+
+outputEl.appendChild(new2El);
