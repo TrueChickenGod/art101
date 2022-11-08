16780200153f2f09016669d9ee07624466152a03
TrueChickenGod <dattaramola@gmail.com>
2022-11-08 15:38:41 -0800

    added fix

--- lab9/js/lab.js
+++ lab9/js/lab.js
@@ -5,7 +5,7 @@
  * Lab 9: JavaScript for the Web
  */
 
-var outputEl = getElementById("output");
+var outputEl = document.getElementById("output");
 
 var new1El = document.createElement("p");
     new1El.innerHTML = "Random";
