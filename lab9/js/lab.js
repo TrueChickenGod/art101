123223e7833d3055b96ef0f4b063802f0300c9c6
TrueChickenGod <dattaramola@gmail.com>
2022-11-08 16:23:08 -0800

    reverted cause didn't work

--- lab9/js/lab.js
+++ lab9/js/lab.js
@@ -7,12 +7,15 @@
 
 var outputEl = document.getElementById("output");
 
-var new1El = document.createElement("p").innerHTML = "Random";
+var new1El = document.createElement("p");
+    new1El.innerHTML = "Random";
 
-var new2El = document.createElement("p").innerHTML = "Again";
+var new2El = document.createElement("p");
+    new2El.innerHTML = "Again";
 
 //adding new DOM elements?
 
 outputEl.appendChild(new1El);
 
 outputEl.appendChild(new2El);
+ 
