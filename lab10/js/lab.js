3f5bed4d8d15dab8c1af46e6cedc155b3105b1a6
TrueChickenGod <dattaramola@gmail.com>
2022-11-14 18:56:36 -0800

    added lab 9 stuffs and lab 10 stuffs

--- lab10/js/lab.js
+++ lab10/js/lab.js
@@ -4,3 +4,21 @@
  * Created Nov. 11, 2022
  * Lab 10: JavaScript Events and Forms
  */
+
+//nameSort function from Lab 7
+
+function nameSort() {
+    var userName = window.prompt("May I steal your username for a moment?");
+    console.log("userName =", userName);
+    //split string to array
+    var nameArray = userName.split('');
+    console.log("nameArray =", nameArray);
+    //sort the array
+    var nameArraySort = nameArray.sort();
+    console.log("nameArraySort =", nameArraySort);
+    //join array back into string
+    var nameSorted = nameArraySort.join('');
+    console.log("nameSorted =", nameSorted);
+
+    return nameSorted;
+}
