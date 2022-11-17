09ae72c57647f522e9edd604bc237623d1014b6a
TrueChickenGod <dattaramola@gmail.com>
2022-11-17 13:14:24 -0800

    ??????

--- lab10/js/lab.js
+++ lab10/js/lab.js
@@ -6,10 +6,7 @@
  */
 
 //nameSort function from Lab 7
-
-var button = document.getElementById("output");
-    buttonEl.addEventListener('click',
-function nameSort() {
+function attemptOne() {
     var userName = window.prompt("May I steal your username for a moment?");
     console.log("userName =", userName);
     //split string to array
@@ -23,13 +20,9 @@ function nameSort() {
     console.log("nameSorted =", nameSorted);
 
     return nameSorted;
+}
+
+document.getElementById("my-button").addEventListener("click", attemptOne() {
+         inputValue = document.getElementById('user-name').value;
+         console.log("You input:", inputValue);
 });
-//replacing div output with results (?)
-//we don't need to add a sperate function here because it calls to a function within the statement anyway(?)
-//otherwise above we would have had to put: function alertClick() { alert("Click again!"); } and take out the function part in the line of code below
-var button   = document.getElementById("theButton");
-    buttonEl.addEventListener('click', function() {
-        inputValue = document.getElementById('user-name').value;
-        console.log("Input your username:", inputValue);
-        nameSort()
-    });
